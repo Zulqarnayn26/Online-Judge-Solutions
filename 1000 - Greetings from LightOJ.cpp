e34537cf18ca692d777543a5f8f07c1b3bfc7c06
Asif <mujtabaasif04@gmail.com>
2018-08-26 06:46:34 +0600

    Beginners Problems

---
+++ 1000 - Greetings from LightOJ.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i = 1;i<=t; i++){
+            int a, b;
+        cin>>a>>b;
+        printf("Case %i: %i\n", i, a+b);
+    }
+}
+	
